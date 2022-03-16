7d2012fc9e5427a91b035b7f78409006f3e29d53
Victor Hugo Camerro <llocusz@gmail.com>
2022-03-16 15:41:57 -0300

    :tada: Challenge Finalized

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+import "./styles/global.scss";
+
+import "./styles/sidebar.scss";
+import "./styles/content.scss";
+
+import { SideBar } from "./components/SideBar";
+import { Content } from "./components/Content";
+import { api } from "./services/api";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
+
+  useEffect(() => {
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        setSelectedGenreId={setSelectedGenreId}
+      />
+
+      <Content
+        selectedGenre={selectedGenre}
+        selectedGenreId={selectedGenreId}
+      />
+    </div>
+  );
+}
